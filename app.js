bfb947d6ec22a71cdfcc4bde05bc9b53560c70fe
손지은 <wiseag@naver.com>
2024-05-23 13:47:09 +0900

    install prsima

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
+
+app.get("/user", (req, res) => {
+  res.send("test");
+});
 
 app.listen(3000, () => console.log("Server Started"));
