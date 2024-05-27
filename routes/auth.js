e412eeb1e1d723f793ba09d4a0e253a77d0dbed1
손지은 <wiseag@naver.com>
2024-05-27 11:17:33 +0900

    feat : login구현

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { CreateUser } from "../structs.js";
+import { CreateUser, LoginUser } from "../structs.js";
 import { assert } from "superstruct";
+import jwt from "jsonwebtoken";
 import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const authRouter = express.Router();
 
+authRouter.get("/test", (req, res) => {
+  res.send(console.log("test"));
+});
 //회원가입
 authRouter.post("/signup", async (req, res) => {
   try {
@@ -21,7 +25,32 @@ authRouter.post("/signup", async (req, res) => {
 });
 
 //로그인
-authRouter.post("/login", async (req, res) => {});
+authRouter.post("/login", async (req, res) => {
+  try {
+    assert(req.body, LoginUser);
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { email, password }
+    });
+    if (user) {
+      const accessToken = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "1d"
+      });
+      const refreshToken = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+      });
+
+      return res.send({
+        accessToken,
+        refreshToken
+      });
+    }
+
+    return res.status(404).send({ status: 404, message: "User not found" });
+  } catch (e) {
+    return res.status(500).send({ message: e.message });
+  }
+});
 
 //로그아웃
 authRouter.post("/logout", async (req, res) => {});
